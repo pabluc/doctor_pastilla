a57a2c910647b53f48deb8561f8b69c0fa220e60
Pablo <pabluc@gmail.com>
2019-03-16 16:59:49 -0300

    Send reminder

--- app/jobs/send_notification_job.rb
+++ app/jobs/send_notification_job.rb
@@ -2,6 +2,12 @@ class SendNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    notification = Notification.where(id: args[0][:id]).last
+
+    if notification.present?
+      SendNotificationJob.set(wait_until: notification.cadence_minutes.minutes.from_now).perform_later({id: notification.id})
+
+      ReminderMailer.config_email(notification).deliver_now
+    end
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,12 @@
 class Notification < ApplicationRecord
   after_create :create_job_confirmation
+  after_create :create_job_reminder
 
   def create_job_confirmation
     SendConfigOkJob.perform_later({id: self.id})
+  end
+
+  def create_job_reminder
+    SendNotificationJob.set(wait_until: self.start).perform_later({id: self.id})
   end
 end
