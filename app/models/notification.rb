f30deebcc5d5e7f8fab2862a437c5acf4f1b6ffe
Pablo <pabluc@gmail.com>
2019-03-16 16:45:43 -0300

    Fix callback Config Ok

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ApplicationRecord
   after_create :create_job_confirmation
 
-  def config_email
+  def create_job_confirmation
     SendConfigOkJob.perform_later({id: self.id})
   end
 end
