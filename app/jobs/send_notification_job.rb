1c2c1ba4aed239fad346f06eefa4df4daa92991e
Pablo <pabluc@gmail.com>
2019-03-16 17:06:26 -0300

    Fix SendNotificationJob

--- app/jobs/send_notification_job.rb
+++ app/jobs/send_notification_job.rb
@@ -7,7 +7,7 @@ class SendNotificationJob < ApplicationJob
     if notification.present?
       SendNotificationJob.set(wait_until: notification.cadence_minutes.minutes.from_now).perform_later({id: notification.id})
 
-      ReminderMailer.config_email(notification).deliver_now
+      ReminderMailer.reminder_email(notification).deliver_now
     end
   end
 end
